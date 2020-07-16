3b1b2ccad522548a715af9e22f74d73c03f4b4e2
LocalizationBuildProcess <v-bruhal@microsoft.com>
2020-07-16 16:08:20 -0700

    Drop the sudo

--- samples/python/02.echo-bot/deploymentScripts/linux/deploy.sh
+++ samples/python/02.echo-bot/deploymentScripts/linux/deploy.sh
@@ -93,18 +93,18 @@ if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
 fi
 
 # 2. Upgrade python version
-upgradePython
+#upgradePython
 
 # 4. Install packages
-echo "python -m pip install --upgrade pip"
-python -m pip install --upgrade pip
+#echo "python -m pip install --upgrade pip"
+#python -m pip install --upgrade pip
 
 echo "python -m pip config set global.extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/"
 python -m pip config set global.extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/
 
 echo "python -m pip install -r requirements.txt --extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/"
 # pip install -r requirements.txt
-python -m pip install -r requirements.txt --extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/
+python -m pip install -r requirements.txt
 # eval $NPM_CMD install --production
 
 exitWithMessageOnError "pip install failed"
