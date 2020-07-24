df9942e33e758bcc283161ff6e5b923ea19078a3
LocalizationBuildProcess <v-bruhal@microsoft.com>
2020-07-24 14:28:56 -0700

    Add export PATH=/home/.local/bin:$PATH

--- samples/python/02.echo-bot/deploymentScripts/linux/deploy.sh
+++ samples/python/02.echo-bot/deploymentScripts/linux/deploy.sh
@@ -118,9 +118,11 @@ python3m --version
 
 echo pip install -U setuptools
 pip install -U setuptools
+#Successfully installed setuptools-44.1.1
 
 echo python3 -m pip install -U setuptools
 python3 -m pip install -U setuptools
+#Successfully installed setuptools-49.2.0
 
 #echo "python -m pip config set global.extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/"
 #python -m pip config set global.extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/
@@ -130,25 +132,36 @@ cd dependencies
 
 echo "python3 -m pip install cryptography==2.8.0"
 python3 -m pip install cryptography==2.8.0
+#Successfully installed cffi-1.14.0 cryptography-2.8 pycparser-2.20 six-1.15.0
 
 echo "python3 -m pip install yarl==1.4.2 -f ./"
 python3 -m pip install yarl==1.4.2 -f ./
+#Successfully installed idna-2.10 multidict-4.7.6 yarl-1.4.2
 
-echo "python3 -m pip install multidict -f ./"
-python3 -m pip install multidict -f ./
+#echo "python3 -m pip install multidict -f ./"
+#python3 -m pip install multidict -f ./
+#Requirement already satisfied: multidict in /home/.local/lib/python3.5/site-packages (4.7.6)
 
 echo "python3 -m pip install aiohttp -f ./"
 python3 -m pip install aiohttp -f ./
+#Successfully installed aiohttp-3.6.2 async-timeout-3.0.1 attrs-19.3.0 chardet-3.0.4 idna-ssl-1.1.0 typing-extensions-3.7.4.2
 
-echo "python3 -m pip install idna-ssl -f ./"
-python3 -m pip install idna-ssl -f ./
+#echo "python3 -m pip install idna-ssl -f ./"
+#python3 -m pip install idna-ssl -f ./
+#Requirement already satisfied: idna>=2.0 in /home/.local/lib/python3.5/site-packages (from idna-ssl) (2.10) 
 
 #echo "python3 -m pip install --requirement ../azure-unresolved-requirements.txt -f ./ --no-index"
 #python3 -m pip install --requirement ../azure-unresolved-requirements.txt -f ./ --no-index
 
 echo "python3 -m pip install --requirement ../requirements.txt -f ./ --no-index"
 python3 -m pip install --requirement ../requirements.txt -f ./ --no-index
+#Successfully installed PyJWT-1.5.3 adal-1.2.1 botbuilder-core-4.10.0.20200724.dev149696 botbuilder-integration-aiohttp-4.10.0.20200724.dev149696 botbuilder-schema-4.10.0.20200724.dev149696 botframework-connector-4.10.0.20200724.dev149696 certifi-2020.6.20 isodate-0.6.0 jsonpickle-1.2 msal-1.2.0 msrest-0.6.10 oauthlib-3.1.0 python-dateutil-2.8.1 requests-2.23.0 requests-oauthlib-1.3.0 urllib3-1.25.10        
+
 # python -m pip install -r requirements.txt --extra-index-url https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/pypi/simple/
+
+echo "This in reponse to WARNING: The script pyjwt is installed in '/home/.local/bin' which is not on PATH."
+echo "export PATH=/home/.local/bin:$PATH"
+export PATH=/home/.local/bin:$PATH
 
 exitWithMessageOnError "pip install failed"
 cd - > /dev/null
